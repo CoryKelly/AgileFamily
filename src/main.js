760ceebff86002ac53dc059409cbce140f5c5fcc
Cory <Kelly>
2020-01-30 17:14:13 -0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import vuetify from "./plugins/vuetify";
+import VueApollo from "vue-apollo";
+
+Vue.use(VueApollo);
+
+const httpLink = new HttpLink({
+  uri: "ADD_YOUR_HASURA_END_POINT_HERE"
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
+  vuetify,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
